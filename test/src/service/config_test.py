85dacd8ef7df4e6963605dfc4294a35ecb1b307c
Gavin <gaprice@lbl.gov>
2022-10-25 10:12:30 -0700

    Add missing noop tests
    Should probably have a corresponding test file for each module

---
+++ test/src/service/config_test.py
@@ -0,0 +1,24 @@
+from src.service.config import CollectionsServiceConfig
+from io import BytesIO
+
+
+# TODO TEST more tests
+
+
+def test_config_minimal():
+    cfg = CollectionsServiceConfig(BytesIO("\n".join([
+        "[Arango]",
+        'url="foo"',
+        'database="bar"',
+        "[Authentication]",
+        'url="foobar"',
+        "[Service]",
+        ]).encode('utf-8')
+    ))
+    assert cfg.arango_url == "foo"
+    assert cfg.arango_db == "bar"
+    assert cfg.arango_user == None
+    assert cfg.arango_pwd == None
+    assert cfg.auth_url == "foobar"
+    assert cfg.auth_full_admin_roles == []
+    assert cfg.service_root_path == None
