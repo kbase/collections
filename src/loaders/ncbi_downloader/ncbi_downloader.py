cb254a4b69c90a3b7b69928a73cdc86f22798007
Tianhao-Gu <tgu@anl.gov>
2022-11-16 10:26:40 -0600

    address comments

--- src/loaders/ncbi_downloader/ncbi_downloader.py
+++ src/loaders/ncbi_downloader/ncbi_downloader.py
@@ -5,15 +5,19 @@ Download genome files from NCBI FTP server
 import os
 import time
 from datetime import datetime
-from typing import List
+
 import ftputil
 import pytz
 
 
-def _download_genome_file(download_dir: str, gene_id: str, target_file_ext: List[str]) -> None:
+def _download_genome_file(download_dir: str, gene_id: str, target_file_ext: list[str]) -> None:
+    # NCBI file structure: a delegated directory is used to store files for all versions of a genome
+    # e.g. File structure for RS_GCF_000968435.2 (https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/968/435/)
+    # genomes/all/GCF/000/968/435/ --> GCF_000968435.1_ASM96843v1/
+    #                              --> GCF_000968435.2_ASM96843v2/
     ncbi_domain, user_name, password = 'ftp.ncbi.nlm.nih.gov', 'anonymous', 'anonymous@domain.com'
 
-    success, attempts, max_attempts = False, 0, 3
+    success, attempts, max_attempts, gene_id = False, 0, 3, gene_id[3:]
     while attempts < max_attempts and not success:
         try:
             time.sleep(5 * attempts)
@@ -39,16 +43,22 @@ def _download_genome_file(download_dir: str, gene_id: str, target_file_ext: List
             attempts += 1
 
     if not success:
-        raise ValueError(f'Download Failed for {gene_id}!')
+        raise ValueError(f'Download Failed for {gene_id} after {max_attempts} attempts!')
 
 
-def download_genome_files(gene_ids: List[str], target_file_ext: List[str], result_dir='ncbi_genomes') -> List[str]:
+def download_genome_files(gene_ids: list[str], target_file_ext: list[str], result_dir='ncbi_genomes') -> list[str]:
+    """
+    Download genome files from NCBI FTP server
+
+    gene_ids: genome ids that parsed from GTDB metadata file (e.g. GB_GCA_000016605.1, RS_GCF_000968435.2).
+    target_file_ext: download only files that match given extensions.
+    result_dir: result direction for downloaded files.
+    """
     print(f'start downloading {len(gene_ids)} genome files')
 
     failed_ids = list()
 
-    if not os.path.isdir(result_dir):
-        os.mkdir(result_dir)
+    os.makedirs(result_dir, exist_ok=True)
 
     counter = 1
     for gene_id in gene_ids:
@@ -56,8 +66,7 @@ def download_genome_files(gene_ids: List[str], target_file_ext: List[str], resul
             print(f"{round(counter / len(gene_ids), 4) * 100}% finished at {datetime.now(pytz.timezone('US/Central'))}")
 
         download_dir = os.path.join(result_dir, gene_id)
-        if not os.path.isdir(download_dir):
-            os.mkdir(download_dir)
+        os.makedirs(download_dir, exist_ok=True)
 
         try:
             _download_genome_file(download_dir, gene_id, target_file_ext)
