9c11636eb42652a0c64201d9e132a841daa6bc6a
Gavin <gaprice@lbl.gov>
2023-01-06 10:49:00 -0800

    Add required data products to matcher spec

--- src/service/matchers/lineage_matcher.py
+++ src/service/matchers/lineage_matcher.py
@@ -5,6 +5,7 @@ Matches assemblies and genomes to collections based on the GTDB lineage string.
 from pydantic import BaseModel, Field
 
 from src.service.matchers.common_models import Matcher
+from src.common.storage.collection_and_field_names import FLD_GENOME_ATTRIBS_GTDB_LINEAGE
 
 class LineageMatcherCollectionParameters(BaseModel):
     "Parameters for the GTDB lineage matcher."
@@ -16,8 +17,11 @@ class LineageMatcherCollectionParameters(BaseModel):
 
 MATCHER = Matcher(
     id="gtdb_lineage",
-    description="Matches based on the GTDB lineage string.",
+    description="Matches based on the GTDB lineage string. Requires the GTDB lineage to be "
+        + f"in the '{FLD_GENOME_ATTRIBS_GTDB_LINEAGE}' field in the genome attributes data "
+        + "product.",
     types=["KBaseGenomes.Genome", "KBaseGenomeAnnotations.Assembly"],
+    required_data_products=["genome_attributes"],
     user_parameters=None, # TODO MATCHERS add rank parameter when supporting rank based matching
     collection_parameters=LineageMatcherCollectionParameters.schema()
 )
