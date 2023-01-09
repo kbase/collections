f451f1d82666688730b2de639440e91c0e73918e
Tianhao-Gu <tgu@anl.gov>
2023-01-09 15:33:03 -0600

    checkout downloader tests

--- test/src/loaders/ncbi_downloader/ncbi_downloader_test.py
+++ test/src/loaders/ncbi_downloader/ncbi_downloader_test.py
@@ -43,15 +43,16 @@ def test_make_work_dir(setup_and_teardown):
     assert path.parents[0].name == source
 
 
-def test_fetch_gtdb_genome_ids(setup_and_teardown, mocker):
+def test_fetch_gtdb_genome_ids(setup_and_teardown):
     tmp_dir, script_file = setup_and_teardown
 
-    mock_form_url = mocker.patch('src.loaders.ncbi_downloader.ncbi_downloader._form_gtdb_taxonomy_file_url')
-    mock_form_url.return_value = ['https://data.gtdb.ecogenomic.org/releases/release202/202.0/ar122_taxonomy_r202.tsv']
+    release_ver = '207'
+    genome_ids = ncbi_downloader._fetch_gtdb_genome_ids(release_ver, tmp_dir)
+    assert len(genome_ids) == 317542
 
     release_ver = '202'
     genome_ids = ncbi_downloader._fetch_gtdb_genome_ids(release_ver, tmp_dir)
-    assert len(genome_ids) == 4316
+    assert len(genome_ids) == 258406
 
 
 def test_parse_gtdb_release_vers():
@@ -69,6 +70,23 @@ def test_form_gtdb_taxonomy_file_url():
 
     assert file_urls_207 == expected_file_urls_207
 
+    file_urls_202 = ncbi_downloader._form_gtdb_taxonomy_file_url('202')
+    expected_file_urls_202 = [f'{GTDB_DOMAIN}release202/202.0/ar122_taxonomy_r202.tsv',
+                              f'{GTDB_DOMAIN}release202/202.0/bac120_taxonomy_r202.tsv']
+
+    assert file_urls_202 == expected_file_urls_202
+
+    file_urls_95 = ncbi_downloader._form_gtdb_taxonomy_file_url('95')
+    expected_file_urls_95 = [f'{GTDB_DOMAIN}release95/95.0/ar122_taxonomy_r95.tsv',
+                             f'{GTDB_DOMAIN}release95/95.0/bac120_taxonomy_r95.tsv']
+
+    assert file_urls_95 == expected_file_urls_95
+
+    file_urls_80 = ncbi_downloader._form_gtdb_taxonomy_file_url('80')
+    expected_file_urls_80 = [f'{GTDB_DOMAIN}release80/80.0/bac_taxonomy_r80.tsv']
+
+    assert file_urls_80 == expected_file_urls_80
+
 
 def test_download_genome_file(setup_and_teardown):
     tmp_dir, script_file = setup_and_teardown
@@ -84,3 +102,4 @@ def test_download_genome_file(setup_and_teardown):
 
     assert len(downloaded_files) == 1
     assert downloaded_files[0].endswith('assembly_report.txt')
+
