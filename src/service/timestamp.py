46d371bab76bdf21e3ab7a45203f42449d431304
Gavin <gaprice@lbl.gov>
2023-01-27 15:06:36 -0800

    Add an endpoint to get match status

--- src/service/timestamp.py
+++ src/service/timestamp.py
@@ -1,9 +1,16 @@
 """
-Contains a function for creating a timestamp.
+Contains functions for creating timestamps.
 """
 
 from datetime import datetime, timezone
+import time
+
 
 def timestamp():
     """ Creates a timestamp from the current time in ISO8601 format. """
     return datetime.now(timezone.utc).isoformat()
+
+
+def now_epoch_millis():
+    """ Returns the current time in Unix epoch milliseconds. """
+    return int(time.time() * 1000)
