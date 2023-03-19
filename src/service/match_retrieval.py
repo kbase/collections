bff4c90942a30a5e05ba353f156f834c4d87b080
Gavin <gaprice@lbl.gov>
2023-03-19 14:01:59 -0700

    Refactor data product processing
    to allow for both match and selection processing

--- src/service/match_retrieval.py
+++ src/service/match_retrieval.py
@@ -264,53 +264,56 @@ def _upaerror(upa, upapath, upapathlen, index):
         raise errors.IllegalParameterError(f"Illegal UPA '{upa}' at index {index}")
 
 
-async def get_or_create_data_product_match(
+# TODO NEXT move to processing.py
+async def get_or_create_data_product_process(
     deps: CollectionsState,
-    match: models.InternalMatch,
+    internal_id: str,
     data_product: str,
-    match_process: Callable[[str, PickleableDependencies, list[Any]], None],
+    type_: models.ProcessType,
+    process_callable: Callable[[str, PickleableDependencies, list[Any]], None],
 ) -> models.DataProductProcess:
     """
-    Get a match process data structure for a data product match.
+    Get a process data structure for a data product process.
 
-    Creates the match and starts the matching process if the match does not already exist.
+    Creates and starts the process the process does not already exist.
 
-    If the match exists but hasn't had a heartbeat in the required time frame, starts another
-    instance of the processes.
+    If the process exists but hasn't had a heartbeat in the required time frame, starts another
+    instance of the process.
 
-    In either case, the list of arguments to the match process is empty.
+    In either case, the list of arguments to the process is empty.
 
     deps - the collections service state.
-    match - the parent match for the data product match.
-    data_product - the data product for the match.
-    match_process - the match process to run if there is no match information in the database or
-        the match heartbeat is too old.
+    internal_id - the internal ID of the parent process for the data product.
+    data_product - the data product for the process.
+    type_ - the type of the process
+    process_callable - the callable to run if there is no process information in the database or
+        the heartbeat is too old.
     """
     now = deps.get_epoch_ms()
-    dp_match, exists = await deps.arangostorage.create_or_get_data_product_process(
+    dp_proc, exists = await deps.arangostorage.create_or_get_data_product_process(
         models.DataProductProcess(
             data_product=data_product,
-            type=models.ProcessType.MATCH,
-            internal_id=match.internal_match_id,
+            type=type_,
+            internal_id=internal_id,
             created=now,
             state=models.ProcessState.PROCESSING,
             state_updated=now,
         )
     )
     if not exists:
-        _start_process(match.match_id, match_process, deps.get_pickleable_dependencies())
-    elif processing.requires_restart(deps.get_epoch_ms(), dp_match):
+        _start_process(internal_id, process_callable, deps.get_pickleable_dependencies())
+    elif processing.requires_restart(deps.get_epoch_ms(), dp_proc):
         logging.getLogger(__name__).warn(
-            f"Restarting match process for match {match.internal_match_id} "
+            f"Restarting {type_.value} process for internal ID {internal_id} "
             + f"data product {data_product}"
         )
-        _start_process(match.match_id, match_process, deps.get_pickleable_dependencies())
-    return dp_match
+        _start_process(internal_id, process_callable, deps.get_pickleable_dependencies())
+    return dp_proc
 
 
 def _start_process(
-    match_id: str,
-    match_process: Callable[[str, PickleableDependencies, list[Any]], None],
+    internal_id: str,
+    process_callable: Callable[[str, PickleableDependencies, list[Any]], None],
     deps: PickleableDependencies,
 ) -> None:
-    processing.CollectionProcess(process=match_process, args=[]).start(match_id, deps)
+    processing.CollectionProcess(process=process_callable, args=[]).start(internal_id, deps)
