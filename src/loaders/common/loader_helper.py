f0dff68f9f003b15913fa458e2e334f14d70b441
Sean Xiang <sijiex@andrew.cmu.edu>
2023-04-10 11:56:23 -0700

    remove extra ,

--- src/loaders/common/loader_helper.py
+++ src/loaders/common/loader_helper.py
@@ -79,7 +79,7 @@ def init_genome_atrri_doc(kbase_collection, load_version, genome_id):
         names.FLD_COLLECTION_ID: kbase_collection,
         names.FLD_LOAD_VERSION: load_version,
         names.FLD_KBASE_ID: genome_id,  # Begin with the input genome_id, though it may be altered by the calling script.
-        names.FLD_MATCHES_SELECTIONS: [],  # for saving matches and selections
+        names.FLD_MATCHES_SELECTIONS: []  # for saving matches and selections
     }
 
     return doc
