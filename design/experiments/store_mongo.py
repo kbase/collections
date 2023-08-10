f992c0fcd011cc72441554a30150fc31376367fb
Gavin <gaprice@lbl.gov>
2023-08-10 16:48:27 -0700

    Get sourmash sigs from mongo and sketch

--- design/experiments/store_mongo.py
+++ design/experiments/store_mongo.py
@@ -7,6 +7,7 @@
 
 import os
 import pymongo
+import subprocess
 import sys
 
 from pathlib import Path
@@ -14,7 +15,7 @@ from pathlib import Path
 
 EXT_SIG = ".sig"
 DB_NAME = "sourmash"
-COLL_NAME = "base64"
+COLL_NAME = "base64"  # this collection name no longer makes sense but whatever, it's test code
 KEY_FILE_SIGNATURE = "sig"
 KEY_FILE_NAME = "file"
 KEY_FILE_LEN = "len"
@@ -33,13 +34,22 @@ def store(coll: pymongo.collection.Collection, sig_path: Path):
                 })
 
 
+def sketch(coll: pymongo.collection.Collection, query_signature: Path, work_dir: Path):
+    for d in coll.find({}, {KEY_FILE_LEN: 0}):
+        with open(work_dir / d[KEY_FILE_NAME], "wb") as f:
+            f.write(d[KEY_FILE_SIGNATURE])
+    subprocess.run(["sourmash", "search", "-n", "0", "-t", "0.5", str(query_signature), str(work_dir)])
+
+
 def main():
     with pymongo.MongoClient() as cli:
+        db = cli.get_database(DB_NAME)
         if sys.argv[1] == "store":
-            store(
-                cli.get_database(DB_NAME).get_collection(COLL_NAME),
-                Path(sys.argv[2])
-            )
+            # args = signature location
+            store(db.get_collection(COLL_NAME), Path(sys.argv[2]))
+        elif sys.argv[1] == "get":
+            # args = query signature location, dir to download target signatures
+            sketch(db.get_collection(COLL_NAME), Path(sys.argv[2]), Path(sys.argv[3]))
 
 
 if __name__ == "__main__":
