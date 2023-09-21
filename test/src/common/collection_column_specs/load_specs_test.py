2dc5fd4859395b0f0aa53643d1a01d8cc2bc002e
Gavin <gaprice@lbl.gov>
2023-09-21 14:16:34 -0700

    Merge column specs for collection data products

--- test/src/common/collection_column_specs/load_specs_test.py
+++ test/src/common/collection_column_specs/load_specs_test.py
@@ -6,9 +6,19 @@ from src.common.product_models.columnar_attribs_common_models import (
     FilterStrategy
 )
 from pathlib import Path
+from pytest import raises
 
 
-def test_all_specs_load():
+# TODO TEST setup more test spec files and do more comprehensive tests checking the entire
+#           spec structure. Allow setting the spec directory to a test dir
+
+
+def test_get_collections_for_dataproduct():
+    assert load_specs.get_collections_for_data_product("genome_attribs"
+        ) == {'PMI', 'GROW', 'ENIGMA', 'GTDB'}
+
+
+def test_all_specs_load_singular():
     # just check the specs' format is ok
     files = list(Path(load_specs.__file__).parent.glob("*.yml"))
     if not files:
@@ -18,6 +28,28 @@ def test_all_specs_load():
         data_product, collection = str(f).split('-')
         collection = collection[:-4]
         load_specs.load_spec(data_product, collection)
+
+
+def test_all_specs_load_merge():
+    st = ColumnType.STRING
+    ft = ColumnType.FLOAT
+    it = ColumnType.INT
+    
+    ident = FilterStrategy.IDENTITY
+    ftext = FilterStrategy.FULL_TEXT
+    
+    spec = load_specs.load_spec("genome_attribs")
+    # just check a few fields
+    key2spec = {c.key: c for c in spec.columns}
+    assert key2spec["kbase_id"] == AttributesColumnSpec(
+        key="kbase_id", type=st, filter_strategy=ident)
+    assert key2spec["classification"] == AttributesColumnSpec(
+            key="classification", type=st, filter_strategy=ftext)
+    assert key2spec["Contamination"] == AttributesColumnSpec(key="Contamination", type=ft)
+    assert key2spec["checkm_contamination"] == AttributesColumnSpec(
+            key="checkm_contamination", type=ft)
+    assert key2spec["translation_table"] == AttributesColumnSpec(
+        key="translation_table", type=it)
 
 
 def test_load_single_spec_from_toolchain():
@@ -32,9 +64,17 @@ def test_load_single_spec_from_toolchain():
         
         assert type(spec) == ColumnarAttributesSpec
         # just check a few fields
-        assert spec.columns[0] == AttributesColumnSpec(key="kbase_id", type=st, filter_strategy=ident)
         key2spec = {c.key: c for c in spec.columns}
+        assert key2spec["kbase_id"] == AttributesColumnSpec(
+            key="kbase_id", type=st, filter_strategy=ident)
         assert key2spec["Contamination"] == AttributesColumnSpec(key="Contamination", type=ft)
         assert key2spec["classification"] == AttributesColumnSpec(
             key="classification", type=st, filter_strategy=ftext)
-        assert key2spec["translation_table"] == AttributesColumnSpec(key="translation_table", type=it)
+        assert key2spec["translation_table"] == AttributesColumnSpec(
+            key="translation_table", type=it)
+
+
+def test_load_key_collision():
+    err = "Column spec conflict for data product test_dp, collections COL2 and COL1 on key bar"
+    with raises(ValueError, match=f"^{err}$"):
+        load_specs.load_spec("test_dp")
