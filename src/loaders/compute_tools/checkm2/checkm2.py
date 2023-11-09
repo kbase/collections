fefa9b255fd7bbf5c6ed2ed92104b6a49759d021
Tianhao-Gu <tgu@anl.gov>
2023-11-09 08:13:44 -0600

    add fatal error file for checkm2

--- src/loaders/compute_tools/checkm2/checkm2.py
+++ src/loaders/compute_tools/checkm2/checkm2.py
@@ -9,6 +9,8 @@ from src.loaders.compute_tools.tool_common import (
     GenomeTuple,
     ToolRunner,
     run_command,
+    write_fatal_tuples_to_dict,
+    create_fatal_tuple,
 )
 from src.loaders.compute_tools.tool_result_parser import (
     process_genome_attri_result,
@@ -47,12 +49,23 @@ def _run_checkm2(
           )
 
     tool_file_name, genome_id_col = 'quality_report.tsv', 'Name'
-    process_genome_attri_result(output_dir,
-                                SELECTED_CHECKM2_FEATURES,
-                                genome_id_col,
-                                ids_to_files,
-                                [tool_file_name],
-                                )
+    genome_attri_docs = process_genome_attri_result(output_dir,
+                                                    SELECTED_CHECKM2_FEATURES,
+                                                    genome_id_col,
+                                                    ids_to_files,
+                                                    [tool_file_name],
+                                                    )
+
+    # create fatal error file if any genome is missing from the result file
+    fatal_tuples = []
+    error_message = f"Result missing in the CheckM2 output file."
+    success_ids = [genome_attri_doc['kbase_id'] for genome_attri_doc in genome_attri_docs]
+    missing_ids = set(ids_to_files.keys()) - set(success_ids)
+    print(f"Found {len(missing_ids)} genomes missing from the CheckM2 output file.")
+    for missing_id in missing_ids:
+        fatal_tuple = create_fatal_tuple(missing_id, ids_to_files, error_message)
+        fatal_tuples.append(fatal_tuple)
+    write_fatal_tuples_to_dict(fatal_tuples, output_dir)
 
 
 def main():
