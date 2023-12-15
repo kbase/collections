5ec8c15b0c0c3887d7c436f253f6fef8e2767ec0
Tianhao-Gu <tgu@anl.gov>
2023-12-14 19:45:09 -0600

    return data product same as before

--- src/service/models.py
+++ src/service/models.py
@@ -295,8 +295,6 @@ class Collection(BaseModel):
         """ Get a data product by its ID. """
         for dp in self.data_products:
             if dp.product == data_product:
-                if generic_view.is_generic_view_product(data_product):
-                    dp.search_view = generic_view.get_generic_view_name(data_product)
                 return dp
         raise ValueError(f"No such data product: {data_product}")
 
