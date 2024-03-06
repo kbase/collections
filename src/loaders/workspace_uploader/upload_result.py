0533f8e85ee80ea2c8ada63f654886c09274e131
Tianhao Gu <Tianhao-Gu@users.noreply.github.com>
2024-03-05 20:45:02 -0600

    Update src/loaders/workspace_uploader/upload_result.py
    Co-authored-by: MrCreosote <MrCreosote@users.noreply.github.com>

--- src/loaders/workspace_uploader/upload_result.py
+++ src/loaders/workspace_uploader/upload_result.py
@@ -12,7 +12,7 @@ from src.common.common_helper import obj_info_to_upa
 WSObjTuple is a named tuple that contains the following fields:
 - obj_name: the name of the object (in many cases, also serves as the file name)
 - obj_coll_src_dir: the directory of the associated file in the collection source directory
-- container_internal_file_dir: the directory of the associated file in the container
+- container_internal_file_dir: the directory of the associated file in the docker container
 """
 WSObjTuple = namedtuple(
     "WSObjTuple",
