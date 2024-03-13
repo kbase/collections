3c0a81890a929fbc5d995d4d606656f5e0588dba
Tianhao-Gu <tgu@anl.gov>
2024-03-13 17:03:43 -0500

    add taskfarmer

--- src/loaders/jobs/taskfarmer/task_generator.py
+++ src/loaders/jobs/taskfarmer/task_generator.py
@@ -49,10 +49,11 @@ running 4 batches in parallel per NERSC node resulted in optimal performance, de
 256 cores.
 '''
 
-TOOLS_AVAILABLE = ['gtdb_tk', 'checkm2', 'microtrait', 'mash']
+TOOLS_AVAILABLE = ['gtdb_tk', 'checkm2', 'microtrait', 'mash', 'eggnog']
 
 # estimated execution time (in minutes) for each tool to process a chunk of data
 TASK_META = {'gtdb_tk': {'chunk_size': 1000, 'exe_time': 65},
+             'eggnog': {'chunk_size': 500, 'exe_time': 65},  # TODO: update this value after performance testing
              'default': {'chunk_size': 5000, 'exe_time': 60}}
 NODE_TIME_LIMIT = 5  # hours  # TODO: automatically calculate this based on tool execution time and NODE_THREADS
 MAX_NODE_NUM = 100  # maximum number of nodes to use
