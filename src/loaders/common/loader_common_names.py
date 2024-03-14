a8330bf69d65a13adcfe05ecc8669af91dc72278
Tianhao Gu <Tianhao-Gu@users.noreply.github.com>
2024-03-14 16:28:26 -0500

    Update src/loaders/common/loader_common_names.py
    Co-authored-by: MrCreosote <MrCreosote@users.noreply.github.com>

--- src/loaders/common/loader_common_names.py
+++ src/loaders/common/loader_common_names.py
@@ -94,7 +94,7 @@ OBJECTS_NAME_GENOME = "KBaseGenomes.Genome"
 # The metadata file name created during the Mash run
 MASH_METADATA = 'mash_run_metadata.json'
 
-# The metadata file name created during the Mash run
+# The metadata file name created during the Eggnog run
 EGGNOG_METADATA = 'eggnog_run_metadata.json'
 
 # The fatal error file created if a data file cannot be successfully processed
